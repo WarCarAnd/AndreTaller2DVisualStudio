007b528fa2b4eb13cf21573aa19ca577e16d37fa
Juan Tamayo <andre.vmunoz@gmail.com>
2026-04-08 19:46:03 -0500

    Tarea Terminada

--- Menu.cs
+++ Menu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,38 +12,56 @@ namespace Tarea
         public void Execute()
 
         {
-            Console.WriteLine("Cual figura quieres operar: ¿Triangulo, Rectangulo o Circulo?");       
-            if (Console.ReadLine() == "Triangulo")
+            bool available = false;
+            while (!available)
             {
+                Console.WriteLine("Cual figura quieres operar: ¿Triangulo, Rectangulo o Circulo?");
+                string figure = Console.ReadLine();
                 float b;
                 float h;
-                Console.WriteLine("Introduce la base");
-                b = float.Parse(Console.ReadLine());
-                Console.WriteLine("Introduce la altura");
-                h = float.Parse(Console.ReadLine());
-                Triangle t = new Triangle(b, h);
-                Console.WriteLine($"El área es {t.CalculateArea()}");
-            }
-            if (Console.ReadLine() == "Rectangulo")
-            {
                 float bas;
-                ; float altura;
-                Console.WriteLine("Introduce la base");
-                bas = float.Parse(Console.ReadLine());
-                Console.WriteLine("Introduce la altura");
-                altura = float.Parse(Console.ReadLine());
-                Rectangulo r = new Rectangulo(bas, altura);
-                Console.WriteLine($"El área es {r.CalculateArea()}");
-            }
-            if (Console.ReadLine() == "Circulo")
-            {
+                float altura;
                 float C;
-                Console.WriteLine("Introduce el radio");
-                C = float.Parse(Console.ReadLine());
-                Circulo R = new Circulo(C);
-                Console.WriteLine($"El área es {R.CalculateArea()}");
-            }
+                switch (figure)
+                {
+                    case "Triangulo":
 
+                        Console.WriteLine("Introduce la base");
+                        b = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduce la altura");
+                        h = float.Parse(Console.ReadLine());
+                        Triangle t = new Triangle(b, h);
+                        Console.WriteLine($"El área es {t.CalculateArea()}");
+                        available = true;
+                        break;
+
+                    case "Rectangulo":
+                        Console.WriteLine("Introduce la base");
+                        bas = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduce la altura");
+                        altura = float.Parse(Console.ReadLine());
+                        Rectangulo r = new Rectangulo(bas, altura);
+                        Console.WriteLine($"El área es {r.CalculateArea()}");
+                        available = true;
+                        break;
+
+                    case "Circulo":
+                        Console.WriteLine("Introduce el radio");
+                        C = float.Parse(Console.ReadLine());
+                        Circulo R = new Circulo(C);
+                        Console.WriteLine($"El área es {R.CalculateArea()}");
+                        available = true;
+                        break;
+                    default:
+                        available = false;
+                        Console.WriteLine("Puedes escribirlo denuevo");
+                        break;
+                }
+            }
         }
     }
-}
+}       
+
+        
+    
+
